53a479447bc3f819fc2a3b8baf95b2ad63ca9ddf
MrNaif2018 <chuff184@gmail.com>
2026-02-16 02:59:42 +0300

    Improve lint job

--- actions/with-post-step/main.js
+++ actions/with-post-step/main.js
@@ -30,19 +30,19 @@ const { appendFileSync } = require("fs");
 const { EOL } = require("os");
 
 function run(cmd) {
-    const subprocess = spawn(cmd, { stdio: "inherit", shell: true });
-    subprocess.on("exit", (exitCode) => {
-        process.exitCode = exitCode;
-    });
+  const subprocess = spawn(cmd, { stdio: "inherit", shell: true });
+  subprocess.on("exit", (exitCode) => {
+    process.exitCode = exitCode;
+  });
 }
 
 const key = process.env.INPUT_KEY.toUpperCase();
 
 if (process.env[`STATE_${key}`] !== undefined) {
-    // Are we in the 'post' step?
-    run(process.env.INPUT_POST);
+  // Are we in the 'post' step?
+  run(process.env.INPUT_POST);
 } else {
-    // Otherwise, this is the main step
-    appendFileSync(process.env.GITHUB_STATE, `${key}=true${EOL}`);
-    run(process.env.INPUT_MAIN);
+  // Otherwise, this is the main step
+  appendFileSync(process.env.GITHUB_STATE, `${key}=true${EOL}`);
+  run(process.env.INPUT_MAIN);
 }
